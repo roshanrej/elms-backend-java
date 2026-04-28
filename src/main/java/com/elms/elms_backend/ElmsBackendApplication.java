3cfefad544bec7841e5dc4822670cfa4d9c9fe29
Roshan Reji Varughese <roshin.r@polus.com>
2026-04-28 17:22:05 +0530

    Initial commit

---
+++ src/main/java/com/elms/elms_backend/ElmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.elms.elms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElmsBackendApplication.class, args);
+	}
+
+}
